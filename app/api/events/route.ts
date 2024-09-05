4f1c05310fee6ee696497c07ed477ba5b576cdb5
silver-or <laura817@naver.com>
2024-09-05 14:09:10 +0900

    0905 commit - event api

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -3,29 +3,29 @@ import type { NextRequest } from 'next/server';
 import axios from 'axios';
 
 const BACKEND_API_URL = process.env.BACKEND_API_URL || 'http://localhost:8080';
+const HEADERS = { 'Content-Type': 'application/json' };
 
+// 모든 일정 가져오기
+export async function GET(request: NextRequest) {
+    try {
+        const { data } = await axios.get(`${BACKEND_API_URL}/api/calendars`, { headers: HEADERS });
+        return NextResponse.json(data, { status: 200 });
+    } catch (error) {
+        console.error('Failed to fetch events:', (error as Error).message);
+        return NextResponse.json({ message: 'Failed to fetch events', error: (error as Error).message }, { status: 500 });
+    }
+}
+
+// 일정 추가
 export async function POST(request: NextRequest) {
     try {
-        // 요청의 JSON 본문을 읽습니다.
         const data = await request.json();
+        console.log('Request Data:', data);
 
-        // 데이터 확인용 로그
-        console.log(data);
-
-        // 백엔드 API 호출
-        const response = await axios.post(`${BACKEND_API_URL}/api/events`, data, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        // 성공적인 응답을 반환합니다.
-        return NextResponse.json({ message: 'Event sent successfully' }, { status: 200 });
+        await axios.post(`${BACKEND_API_URL}/api/calendars`, data, { headers: HEADERS });
+        return NextResponse.json({ message: 'Event added successfully' }, { status: 200 });
     } catch (error) {
-        // 에러 로깅
-        console.error('Error sending event:', error);
-
-        // 에러 메시지를 반환합니다.
-        return NextResponse.json({ message: 'Failed to send event', error: (error as Error).message }, { status: 500 });
+        console.error('Failed to add event:', (error as Error).message);
+        return NextResponse.json({ message: 'Failed to add event', error: (error as Error).message }, { status: 500 });
     }
 }
