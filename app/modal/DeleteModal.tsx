e59410d791cd430cbe364651b980a72909ef0366
silver-or <laura817@naver.com>
2024-09-06 15:57:44 +0900

    0906 commit - delete 구현 완료

--- app/modal/DeleteModal.tsx
+++ app/modal/DeleteModal.tsx
@@ -4,10 +4,16 @@ import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDi
 interface DeleteModalProps {
     isOpen: boolean;
     onClose: () => void;
+    onDelete: () => void;
 }
 
-const DeleteModal: React.FC<DeleteModalProps> = ({ isOpen, onClose }) => {
+const DeleteModal: React.FC<DeleteModalProps> = ({ isOpen, onClose, onDelete }) => {
     const { onOpenChange } = useDisclosure();
+
+    const handleDelete = () => {
+        onDelete(); // 3
+        onClose();
+    };
 
     return (
         <>
@@ -32,14 +38,14 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ isOpen, onClose }) => {
                                 </p>
                                 <p className="text-gray-600">
                                     If you delete the schedule <br />
-                                    you cant recover it.
+                                    you can't recover it.
                                 </p>
                             </ModalBody>
                             <ModalFooter className="justify-center">
                                 <Button className='text-gray-700' variant="light" onPress={onClose}>
                                     Close
                                 </Button>
-                                <Button color="primary" onPress={onClose}>
+                                <Button color="primary" onPress={handleDelete}>
                                     Delete
                                 </Button>
                             </ModalFooter>
@@ -52,3 +58,6 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ isOpen, onClose }) => {
 }
 
 export default DeleteModal;
+
+// 최하위 컴포넌트(DeleteModal) 에서 상위 컴포넌트(EventCard)로 버블링 → 최상위 컴포넌트(Home)까지 도달하여 전체 상태 업데이트
+// 상태 업데이트로 인한 변경사항이 다시 하위 컴포넌트로 캡처링되어 내려가는 형태로 UI 업데이트
