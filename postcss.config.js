900a0756e35841de7b8ab2a45d6e0d45b9de9362
silver-or <laura817@naver.com>
2024-09-19 10:01:19 +0900

    0919 commit - postcss.config.js 수정

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,7 @@
 // postcss.config.js
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './src/**/*.html',
-                './src/**/*.js',
-                './src/**/*.jsx',
-                './src/**/*.ts',
-                './src/**/*.tsx',
-            ],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        }),
-        require('autoprefixer'),
-    ]
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 }
